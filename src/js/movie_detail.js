75603a07eae7b7c445b338b8565a00d118dafdd8
jorgechvz <cha22060@byui.edu>
2023-07-04 18:42:27 -0500

    Movie Detail 70/100 complete

--- src/js/movie_detail.js
+++ src/js/movie_detail.js
@@ -1,3 +1,11 @@
+import { getTrailer, movieDetail } from "./movieDetail.mjs";
 import { getParam } from "./utils.mjs";
 
+
+
 const movieId = getParam("movie");
+const selector = ".movie-detail"
+
+movieDetail(movieId, selector)
+
+getTrailer(movieId)
