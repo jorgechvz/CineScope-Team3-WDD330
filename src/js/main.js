22b663d181b2bf565c836e0fac2a32fde4944d8d
jorgechvz <cha22060@byui.edu>
2023-07-19 22:50:55 -0500

    Fix footer in main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { mainMovie } from "./main.mjs";
-import { getAccount, logout } from "./authenticate.mjs";
 import { btnLoginLogout, loadHeaderFooter } from "./utils.mjs";
+
 loadHeaderFooter();
 
 btnLoginLogout();
