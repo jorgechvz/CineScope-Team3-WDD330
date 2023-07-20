8836121f38b21948718aaab3a65a75b1cc1f3d48
jorgechvz <cha22060@byui.edu>
2023-07-19 22:34:47 -0500

    Fixed dist

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
         list_movies: resolve(__dirname, "src/list_movies/index.html"),
         movie_detail: resolve(__dirname, "src/movie_detail/index.html"),
         login: resolve(__dirname, "src/login/index.html")
